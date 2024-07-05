b31ea1b6ab1b9552cb5ad258642ba37cc495e005
jaybhoyar <jaybhoyar1997@gmail.com>
2024-07-05 17:49:18 +0530

    Add spec for condtion that returns 0 for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,4 +3,9 @@ require 'rspec'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  describe '.add' do
+    it "returns 0 for an empty string" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+  end
 end
